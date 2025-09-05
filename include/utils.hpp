5810a0aa7dcd0e28627396724893378d213286a3
Vezeteu Eugeniu <53303767+eugeniu1994@users.noreply.github.com>
2025-09-05 15:55:32 +0300

    Add files via upload

--- include/utils.hpp
+++ include/utils.hpp
@@ -23,10 +23,18 @@
 #include <csignal>
 #include <unistd.h>
 #include <ros/ros.h>
-#include <chrono>
+#include <string>
+#include <deque>
+#include <fstream>
+#include <memory>
+#include <vector>
+
 
 #include <tbb/parallel_for.h>
 #include <tsl/robin_map.h>
+
+
+
 
 struct EIGEN_ALIGN16 PointType
 {
@@ -60,19 +68,24 @@ struct VoxelAccum {
     PointType max_point; 
 };
 
+std::vector<float> read_lidar_buffer(const std::string &lidar_data_path)
+{
+    /*
+        function to take the scan path and read the binary data 
+        return a vector of floats
+    */
+    std::ifstream lidar_data_file(lidar_data_path, std::ifstream::in | std::ifstream::binary);
+    lidar_data_file.seekg(0, std::ios::end);
+    const size_t num_elements = lidar_data_file.tellg() / sizeof(float);
+    lidar_data_file.seekg(0, std::ios::beg);
+
+    std::vector<float> lidar_data_buffer(num_elements);
+    lidar_data_file.read(reinterpret_cast<char *>(&lidar_data_buffer[0]), num_elements * sizeof(float));
+    return lidar_data_buffer;
+}
 
 const int kernel_row = 1, kernel_col = 5;
 
-
-// config of the hesai
-const int N_SCAN = 32;         // number of vertical channels
-const float ang_res_x = 0.18f; // horizontal angular resolution in degrees
-const int Horizon_SCAN = 2000;  // 360° / ang_res_x° resolution
-
-//config of the hdl-64
-// const int N_SCAN = 64;
-// const float ang_res_x = 0.2; // horizontal angular resolution in degrees
-// const int Horizon_SCAN = 1800; 
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "utils.hpp"
 
+volatile bool flg_exit = false;
+void SigHandle(int sig)
+{
+    ROS_WARN("Caught signal %d, stopping...", sig);
+    flg_exit = true; // Set the flag to stop the loop
+}
+
 class FPR
 {
 public:
@@ -11,13 +18,15 @@ public:
         nh.param<std::string>("output_topic_voxelized", output_topic_voxelized, "/fpr_voxelized");
         nh.param<std::string>("output_topic_filtered", output_topic_filtered, "/fpr_filtered");
         nh.param<double>("voxel_size", voxel_size, .5);
+
         
-        sub_ = nh.subscribe(input_topic_, 10, &FPR::cloudCallback, this);
         pub_full = nh.advertise<sensor_msgs::PointCloud2>(output_topic_fpr, 100);
         pub_voxelized = nh.advertise<sensor_msgs::PointCloud2>(output_topic_voxelized, 100);
         pub_filtered = nh.advertise<sensor_msgs::PointCloud2>(output_topic_filtered, 100);
 
-        ROS_INFO("Subscribed to: %s", input_topic_.c_str());
+        nh.param<bool>("from_bag", from_bag_, false);
+        std::cout<<"from_bag_:"<<from_bag_<<std::endl;
+        
         ROS_INFO("Publishing full to: %s", output_topic_fpr.c_str());
         ROS_INFO("Publishing voxelized to: %s", output_topic_voxelized.c_str());
         ROS_INFO("Publishing filtered to: %s", output_topic_filtered.c_str());
@@ -33,9 +42,158 @@ public:
                 _kernel.push_back(std::make_pair(i, j));
             }
         }
+
+        if(from_bag_)
+        {
+            sub_ = nh.subscribe(input_topic_, 10, &FPR::cloudCallback, this);
+            ROS_INFO("Subscribed to: %s", input_topic_.c_str());
+
+            //todo - read this from some param file
+            N_SCAN = 32;         // number of vertical channels
+            ang_res_x = 0.18f; // horizontal angular resolution in degrees
+            Horizon_SCAN = 2000;  // 360° / ang_res_x° resolution
+        }
+        else 
+        {
+            nh.param<std::string>("bin_file", bin_file_, "");
+            std::cout<<"Read from "<<bin_file_<<std::endl;
+            //config of the hdl-64
+            N_SCAN = 64;
+            ang_res_x = 0.2; // horizontal angular resolution in degrees
+            Horizon_SCAN = 1800; 
+            processFolder(bin_file_);
+        }
     }
 
 private:
+
+    void processFolder(std::string &dataset_folder)
+    {
+        std::string sequence = "03"; // sset the desired scenario (Note, there is no 01 and 08 scenarios)
+
+        // set the fog, rain or snow
+        // std::string dataset_folder = "/your_path_goes_here/data_3d_augmented/2013_05_28_drive_00" + sequence + "_sync/augmented_fog";
+        // std::string dataset_folder = "/your_path_goes_here/data_3d_augmented/2013_05_28_drive_00" + sequence + "_sync/augmented_rain";
+        // std::string dataset_folder = "/your_path_goes_here/data_3d_augmented/2013_05_28_drive_00" + sequence + "_sync/augmented_snow";
+
+        std::string timestamp_path = dataset_folder + "timestamps.txt";                                                       // the timestmps for all the scans
+        std::cout << "dataset_folder: " << dataset_folder << std::endl;
+        std::cout << "timestamp_path: " << timestamp_path << std::endl;
+
+        std::ifstream timestamp_file(timestamp_path, std::ifstream::in);
+
+        std::string line;
+        std::size_t line_num = 0;
+        float timestamp = 0.;
+        int scan = 0;
+        ros::Time ros_timestamp;        
+    
+        // if (sequence == "02")
+        // {
+        //     line_num = 4391;
+        // }
+
+        signal(SIGINT, SigHandle); // Handle Ctrl+C (SIGINT)
+        flg_exit = false;
+
+        int point_dim = 6; //[x,y,z,intensity,ring,time]
+        while (std::getline(timestamp_file, line))
+        {
+            if (!ros::ok())
+            {
+                break;
+            }
+
+            if (flg_exit)
+                break;
+
+            timestamp += .1; // you could take the actual time from line - std::getline(timestamp_file, line)
+            ros_timestamp.fromSec(timestamp);
+
+            std::cout << "\nscan:" << scan << std::endl;
+            scan++;
+
+            std::stringstream lidar_data_path;
+
+            // fog version
+            lidar_data_path << dataset_folder + "augmented_fog/" << std::setfill('0') << std::setw(10) << line_num << ".bin";
+
+            // snow version
+            // lidar_data_path << dataset_folder + "augmented_snow/" << std::setfill('0') << std::setw(10) << line_num << ".bin";
+
+            // rain version
+            // lidar_data_path << dataset_folder + "augmented_rain/" << std::setfill('0') << std::setw(10) << line_num << ".bin";
+
+            std::vector<float> lidar_data;
+            lidar_data = read_lidar_buffer(lidar_data_path.str()); // read the lidar data
+            PointCloudXYZI::Ptr cloud(new PointCloudXYZI);
+            PointCloudXYZI::Ptr cloud_trimmed(new PointCloudXYZI);
+            PointCloudXYZI::Ptr cloud_voxelized(new PointCloudXYZI);
+            PointCloudXYZI::Ptr filtered(new PointCloudXYZI);
+
+            for (std::size_t i = 0; i < lidar_data.size(); i += point_dim)
+            {
+                float x = lidar_data[i];
+                float y = lidar_data[i + 1];
+                float z = lidar_data[i + 2];
+                float intensity = lidar_data[i + 3];
+                float ring = lidar_data[i + 4];
+                float time = lidar_data[i + 5];
+
+                PointType p;
+                p.x = x;
+                p.y = y;
+                p.z = z;
+                p.intensity = intensity;
+                p.timestamp = time;
+                p.ring = ring;
+
+                cloud->push_back(p);
+            }
+
+            line_num++;
+
+            std::cout<<"cloud:"<<cloud->size()<<std::endl;
+            // 1. range image projection
+            Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic> Neighbours;
+            Neighbours.resize(N_SCAN, Horizon_SCAN);
+            projectToRangeImage(cloud, cloud_trimmed, Neighbours);
+
+            // 2.rank estimation
+            computeRank(cloud, cloud_trimmed, Neighbours);
+
+            // 3. rank based voxelization
+            bool use_weighted = true; // false;
+            Voxelize(cloud_trimmed, cloud_voxelized, voxel_size, use_weighted);
+            // std::cout<<"\ncloud_voxelized:"<<cloud_voxelized->size()<<std::endl;
+
+            // 4. example filter x% lowest rank points
+            FilterByIntensityPercent(cloud_trimmed, filtered, 10.0); // drop 10%
+
+            // publish the clouds
+            sensor_msgs::PointCloud2 out_msg, out_msg2, out_msg3;
+            pcl::toROSMsg(*cloud_trimmed, out_msg);    // full cloud with fpr
+            pcl::toROSMsg(*cloud_voxelized, out_msg2); // voxelized
+            pcl::toROSMsg(*filtered, out_msg3);        // filtered
+
+            sensor_msgs::PointCloud2 msg;
+            msg.header.frame_id = "Velodyne";  
+            msg.header.stamp = ros_timestamp;
+
+            out_msg.header = msg.header;
+            out_msg2.header = msg.header;
+            out_msg3.header = msg.header;
+
+            pub_full.publish(out_msg);
+            pub_voxelized.publish(out_msg2);
+            pub_filtered.publish(out_msg3);
+
+            int wait_time_ms = 50; // 0.05 second 
+            cv::waitKey(wait_time_ms);
+        }
+        cv::destroyAllWindows();
+    }
+
     void cloudCallback(const sensor_msgs::PointCloud2ConstPtr &msg)
     {
         PointCloudXYZI::Ptr cloud(new PointCloudXYZI);
@@ -57,16 +215,16 @@ private:
         // 3. rank based voxelization
         bool use_weighted = true; // false;
         Voxelize(cloud_trimmed, cloud_voxelized, voxel_size, use_weighted);
-        //std::cout<<"\ncloud_voxelized:"<<cloud_voxelized->size()<<std::endl;
+        // std::cout<<"\ncloud_voxelized:"<<cloud_voxelized->size()<<std::endl;
 
-        //4. example filter x% lowest rank points
+        // 4. example filter x% lowest rank points
         FilterByIntensityPercent(cloud_trimmed, filtered, 10.0); // drop 10%
 
         // publish the clouds
         sensor_msgs::PointCloud2 out_msg, out_msg2, out_msg3;
-        pcl::toROSMsg(*cloud_trimmed, out_msg);     // full cloud with fpr
-        pcl::toROSMsg(*cloud_voxelized, out_msg2);  // voxelized
-        pcl::toROSMsg(*filtered, out_msg3);         // filtered
+        pcl::toROSMsg(*cloud_trimmed, out_msg);    // full cloud with fpr
+        pcl::toROSMsg(*cloud_voxelized, out_msg2); // voxelized
+        pcl::toROSMsg(*filtered, out_msg3);        // filtered
 
         out_msg.header = msg->header;
         out_msg2.header = msg->header;
@@ -209,10 +367,10 @@ private:
             int row, col;
             if (projectPoint(pt, row, col))
             {
-                densityMat.at<uchar>(row, col) = pt.intensity * 240.; // just for display
+                densityMat.at<uchar>(row, col) = pt.intensity * 100;// 64.; // just for display
             }
         }
-        //  applyColorMap(show_image, show_image, cv::COLORMAP_SUMMER);  //COLORMAP_RAINBOW
+        //cv::applyColorMap(densityMat, show_image, cv::COLORMAP_SUMMER); 
         cv::applyColorMap(densityMat, show_image, cv::COLORMAP_RAINBOW); // COLORMAP_RAINBOW
         // cv::flip(show_image, show_image, 0);
         cv::imshow("FPR", show_image);
@@ -300,7 +458,7 @@ private:
         // How many points to drop
         size_t numToDrop = static_cast<size_t>((percentage_to_drop / 100.0) * points.size());
 
-        //put the lowest-intensity points in the first numToDrop positions
+        // put the lowest-intensity points in the first numToDrop positions
         std::partial_sort(points.begin(),
                           points.begin() + numToDrop,
                           points.end(),
@@ -324,12 +482,23 @@ private:
 
     ros::Subscriber sub_;
     ros::Publisher pub_full, pub_voxelized, pub_filtered;
-    std::string input_topic_;
+    std::string input_topic_, bin_file_;
     std::string output_topic_fpr, output_topic_voxelized, output_topic_filtered;
 
     float max_range = 100;
     double voxel_size = .5;
     std::vector<std::pair<int, int>> _kernel;
+    bool from_bag_ = false;
+
+    // config of the hesai
+    int N_SCAN = 32;         // number of vertical channels
+    float ang_res_x = 0.18f; // horizontal angular resolution in degrees
+    int Horizon_SCAN = 2000;  // 360° / ang_res_x° resolution
+
+    //config of the hdl-64
+    //int N_SCAN = 64;
+    //float ang_res_x = 0.2; // horizontal angular resolution in degrees
+    //int Horizon_SCAN = 1800; 
 };
 
 int main(int argc, char **argv)
